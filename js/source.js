13b34f535fa5032dfe21b8358c3d10f99c26f99e
fujun-iri <fujun-iri@360.cn>
2016-10-14 09:54:37 +0800

    revise invalid selector

--- js/source.js
+++ js/source.js
@@ -33,7 +33,7 @@ var sourceList = {
         "colum": ".thtrm-three-column-list li",
         "title": "h2",
         "time": "",
-        "link": ".thtrm-article-card-content a",
+        "link": ".thtrm-cta.thtrm-cta--small",
         "max": 5
     },
     "W3cplus": {
