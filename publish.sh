19ff93badbc2391bb32e4b9706f6a415bbac24e0
wemlion <angusfu1126@qq.com>
2016-11-13 00:22:59 +0800

    css

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# https://github.com/vuejs/vue/blob/dev/build/release.sh
+set -e
+echo "Enter commit message: "
+read msg
+
+read -p "Are you sure to commit (y/n)" -n 1 -r
+
+
+if [[ $REPLY =~ ^[Yy]$ ]] 
+then
+  # commit master
+  git init
+  git add -A
+  git commit -m "$msg"
+  git push
+  echo "master sync succeeded..."
+else
+  echo
+  echo "Operation is abandoned"
+
+fi
